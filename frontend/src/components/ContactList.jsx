b8c7e8eb2cd1c4cc6a4cc77d957dc7a62ba1c2eb
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-19 14:15:44 +0530

    add status filter and search input to ContactList component

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -2,17 +2,23 @@ import React, { useState } from "react";
 
 const ContactList = ({ contacts, setContacts }) => {
   const [search, setSearch] = useState("");
+  const [statusFilter, setStatusFilter] = useState("All Status");
 
   return (
     <div>
       <div className="flex gap-10">
-        <select name="" id=""></select>
+        <select className="p-2 rounded bg-[#00277a] text-white cursor-pointer outline-0" value={statusFilter} onChange={(e) => setStatusFilter(e.target.value)}>
+          <option value="All Status">All Status</option>
+          <option value="Interested">Interested</option>
+          <option value="Follow-up">Follow-up</option>
+          <option value="Closed">Closed</option>
+        </select>
         <input
           type="text"
           placeholder="Search by name or company"
           className="p-3 rounded w-full bg-[#eff4ff] outline-0"
           value={search}
-          onChange={(e) => setSearch(e.target.value)} 
+          onChange={(e) => setSearch(e.target.value)}
         />
       </div>
     </div>
