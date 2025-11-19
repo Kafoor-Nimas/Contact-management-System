ab937c3a853a922a1fa8dfdb0f4e73d82e23261b
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-19 11:04:54 +0530

    implement main App component with ContactForm and ContactList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
- import React from 'react'
- 
- const App = () => {
-   return (
-     <div>App</div>
-   )
- }
- 
- export default App
+import React from "react";
+import ContactList from "./components/ContactList";
+import ContactForm from "./components/ContactForm";
+
+const App = () => {
+  const [contacts, setContacts] = React.useState([]);
+
+  return (
+    <div>
+      <div className="p-8 max-w-[1440px] mx-auto grid grid-cols-3 gap-[70px]">
+        <div className="col-span-1 space-y-4">
+          <h1 className="text-[32px] font-bold mb-10 text-[#00277a]">
+            Contact Management
+          </h1>
+          <ContactForm contacts={contacts} setContacts={setContacts} />
+        </div>
+        <div className="col-span-2">
+          <ContactList contacts={contacts} setContacts={setContacts} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/ContactForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactForm = ({ contacts, setContacts }) => {
+  return (
+    <div>ContactForm</div>
+  )
+}
+
+export default ContactForm
