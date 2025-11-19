dd2dd8cfc4df21cb1b9474fa1a395a5ac9ecffc1
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-19 07:48:06 +0530

    contact schema model created

---
+++ backend/models/contact.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: String,
+  phone: String,
+  company: String,
+  status: {
+    type: String,
+    enum: ["Intrested", "Follow-up", "Closed"],
+    default: "Intrested",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Contact", contactSchema);
--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// app.get("/", (req, res) => {
+//   res.send("Server working fine...");
+// });
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
   })
   .catch((error) => console.log(error));
